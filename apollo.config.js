d117d763f6276c710959e3f125b22e7d90834166
benoitDD <benoitdegorsas@outlook.fr>
2019-05-27 14:32:04 +0200

    configuation vsc plugin: look only on query directory

--- apollo.config.js
+++ apollo.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 module.exports = {
     client: {
+        includes: ['./src/swapi/query/*'],
         service: {
             name: 'swapi',
             localSchemaFile: './swapi.json'
